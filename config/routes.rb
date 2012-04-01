b862ce64c8b976c85f889f5bfc6c10feacc81400
Brian Harris <aircav11@gmail.com>
2012-04-01 16:00:44 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ SampleApp::Application.routes.draw do
   # get "static_pages/contact"
   
   root to: 'static_pages#home'
+  match '/signup',  to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
